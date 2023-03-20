4a909b706dd3f4f1d17a4a668ea9f69108482a02
Kirill Petrunin <petruninkir@gmail.com>
2023-03-20 14:38:58 +0400

    add file handling

---
+++ services/file_handling.py
@@ -0,0 +1,33 @@
+BOOK_PATH = 'book/book.txt'
+# BOOK_PATH = '../book/book.txt'
+PAGE_SIZE = 1050
+
+book: dict = {}
+
+def _get_part_text(text: str, start: int, page_size: int) -> tuple[str, int]:
+    max_text = text[start:start + page_size]
+    if max_text.endswith('!.') or max_text.endswith('?.') or max_text.endswith('..'):
+        max_text = max_text.strip('!?.')
+    punctuation = ',.!?:;'
+    while max_text[-1] not in punctuation:
+        max_text = max_text[:-1]
+    return (max_text, len(max_text))
+
+
+
+def prepare_book(path: str) -> None:
+    start: int = 0
+    num_str: int = 1
+    page_size: int = PAGE_SIZE
+    with open(path, 'r', encoding='utf-8') as text:
+        text = text.read()
+        finish = len(text)
+        while start < finish:
+            text_for_dict, len_text = _get_part_text(text, start, page_size)
+            book[num_str] = text_for_dict.lstrip()
+            start += len_text
+            num_str += 1
+        
+
+
+# prepare_book(BOOK_PATH)
