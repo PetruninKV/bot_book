49511b0fd64f9e13cde088f7b2bf2df4079fd1d6
Kirill Petrunin <petruninkir@gmail.com>
2023-03-20 15:06:38 +0400

    add main menu keyboard

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import logging
 
 from aiogram import Bot, Dispatcher
 from config_data.config import Config, load_config
+from keyboards.main_menu import set_main_menu
 
 
 
@@ -23,7 +24,7 @@ async def main():
     bot: Bot = Bot(token=config.tg_bot.token, parse_mode='HTML')
     dp: Dispatcher = Dispatcher()
 
-    # await set_main_menu(bot)
+    await set_main_menu(bot)
 
     # dp.include_router(user_handlers.router)
     # dp.include_router(other_handlers.router)
