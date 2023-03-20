31fad4b7bac486cc030c6f6fcbf29eb151b868eb
Kirill Petrunin <petruninkir@gmail.com>
2023-03-20 22:21:25 +0400

    add handler start command

---
+++ database/database.py
@@ -0,0 +1,5 @@
+user_dict_template: dict = {'page': 1,
+                            'bookmarks': set()}
+
+
+users_db: dict = {}
---
+++ handlers/user_handlers.py
@@ -0,0 +1,23 @@
+from copy import deepcopy
+
+from aiogram import Router
+from aiogram.filters import Command, CommandStart, Text
+from aiogram.types import CallbackQuery, Message
+from database.database import user_dict_template, users_db
+from filters.filters import IsDelBookmarkCallbackData, IsDigitCallbackData
+from keyboards.bookmarks_kb import (create_bookmarks_keyboard, create_edit_keyboard)
+from keyboards.pagination_kb import create_pagination_keyboard
+from lexicon.lexicon import LEXICON
+from services.file_handling import book
+
+
+router: Router = Router()
+
+
+@router.message(CommandStart)
+async def start_command(message: Message):
+    await message.answer(text=LEXICON['/start'])
+    if message.from_user.id not in users_db:
+        users_db[message.from_user.id] = deepcopy(user_dict_template)
+
+
